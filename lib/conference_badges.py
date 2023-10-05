d151138f34a1d24f9c0ed1ea33a5d121d91955a1
Wilson Wanjiru <wilsonmuita41@gmail.com>
2023-10-05 19:02:08 +0300

    Badges and Schedules Lab

--- lib/conference_badges.py
+++ lib/conference_badges.py
@@ -1,11 +1,23 @@
 def badge_maker(name):
-    return None
+    return f"Hello, my name is {name}."
 
 def batch_badge_creator(names):
-    return None
+    badges = []
+    for name in names:
+        badges.append(badge_maker(name))
+    return badges
 
-def assign_rooms(names):
-    return None
+def assign_rooms(speakers):
+    room_assignments = []
+    for i, speaker in enumerate(speakers, start=1):
+        room_assignments.append(f"Hello, {speaker}! You'll be assigned to room {i}!")
+    return room_assignments
 
-def printer(names):
-    return None
+def printer(speakers):
+    badges = batch_badge_creator(speakers)
+    for badge in badges:
+        print(badge)
+    assignments = assign_rooms(speakers)
+    for assignment in assignments:
+        print(assignment)
+
